ec40f89c82439acedeab251eba8b0d2bc48941cb
Jeremiah Booth <jeremiah.boothphotog@gmail.com>
2021-04-30 09:24:13 -0700

    Made all sub functions

--- index.js
+++ index.js
@@ -25,3 +25,45 @@ function getDestination () {
   }
 }
 
+function getRestaurants () {
+  let confirmed = false;
+  while(confirmed === false){
+    let randNum = Math.floor(Math.random() * 5);
+    let randRestaurants = restaurants[randNum];
+    let userConfrim = prompt(`Your restaurants is ${randRestaurants}, Is this restaurant exceptable? (y/n):`);
+
+    if (userConfrim == "y") {
+    confirmed = true;
+    return randRestaurant;
+    } 
+  }
+}
+
+function getTransportation () {
+  let confirmed = false;
+  while(confirmed === false){
+    let randNum = Math.floor(Math.random() * 5);
+    let randTrasportation = transportation[randNum];
+    let userConfrim = prompt(`Your trasportation is ${randTrasportation}, Is this trasportaion exceptable? (y/n):`);
+
+    if (userConfrim == "y") {
+    confirmed = true;
+    return randTrasportation;
+    } 
+  }
+}
+
+function getEntertainment () {
+  let confirmed = false;
+  while(confirmed === false){
+    let randNum = Math.floor(Math.random() * 5);
+    let randEntertainment = entertainment[randNum];
+    let userConfrim = prompt(`Your destination is ${randEntertainment}, Is this destination exceptable? (y/n):`);
+
+    if (userConfrim == "y") {
+    confirmed = true;
+    return randEntertainment;
+    } 
+  }
+}
+
