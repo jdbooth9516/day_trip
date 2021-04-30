de122c517cf083f077263737fac86acc2c776a77
Jeremiah Booth <jeremiah.boothphotog@gmail.com>
2021-04-30 10:20:29 -0700

    made it so that the choices dont repeat

--- index.js
+++ index.js
@@ -14,59 +14,83 @@ let entertainment = ["Tourist-site", 'Show', 'Urban-Exploration', "Trainride", "
 // function for pulling a random destination
 function getDestination () {
   let confirmed = false;
+  let destinationNotWanted = '';
+
   while(confirmed === false){
     let randNum = Math.floor(Math.random() * destinations.length);
     let randDestinations = destinations[randNum];
-    let userConfrim = prompt(`Your destination is ${randDestinations}, Is this destination exceptable? (y/n):`);
-
-    if (userConfrim == "y") {
+    let userConfirm = '';
+    if(randDestinations !== destinationNotWanted){
+      userConfirm = prompt(`Your destination is ${randDestinations}, Is this destination exceptable? (y/n):`);
+   }
+    if (userConfirm == "y") {
     confirmed = true;
     return randDestinations;
-    } 
+    } else {
+      destinationNotWanted = randDestinations;
+    }
   }
 }
 // function for pulling a random restaurants
 function getRestaurants () {
   let confirmed = false;
+  let restaurentNotWanted = '';
+
   while(confirmed === false){
     let randNum = Math.floor(Math.random() * restaurants.length);
     let randRestaurants = restaurants[randNum];
-    let userConfrim = prompt(`Your restaurants is ${randRestaurants}, Is this restaurant exceptable? (y/n):`);
-
-    if (userConfrim == "y") {
+    let userConfirm = '';
+    if(randRestaurants !== restaurentNotWanted){
+      userConfirm = prompt(`Your restaurants is ${randRestaurants}, Is this restaurant exceptable? (y/n):`);
+    }
+    if (userConfirm == "y") {
     confirmed = true;
     return randRestaurants;
-    } 
+    } else {
+      restaurentNotWanted = randRestaurants;
+    }
   }
 }
 
 // function for pulling the trasportation
 function getTransportation () {
   let confirmed = false;
+  let transportNotWanted = '';
   while(confirmed === false){
     let randNum = Math.floor(Math.random() * transportation.length);
     let randTrasportation = transportation[randNum];
-    let userConfrim = prompt(`Your trasportation is ${randTrasportation}, Is this trasportaion exceptable? (y/n):`);
+    let userConfirm = '';
+    if(randTrasportation !== transportNotWanted){
+       userConfirm = prompt(`Your trasportation is ${randTrasportation}, Is this trasportaion exceptable? (y/n):`);
+    }
 
-    if (userConfrim == "y") {
+    if (userConfirm == "y") {
     confirmed = true;
     return randTrasportation;
-    } 
+    } else {
+      transportNotWanted = randTrasportation;
+    }
   }
 }
 
 // function for pulling the entertainment
 function getEntertainment () {
   let confirmed = false;
+  let entertainmentNotWanted = '';
+
   while(confirmed === false){
     let randNum = Math.floor(Math.random() * entertainment.length);
     let randEntertainment = entertainment[randNum];
-    let userConfrim = prompt(`Your destination is ${randEntertainment}, Is this destination exceptable? (y/n):`);
-
-    if (userConfrim == "y") {
+    let userConfirm = '';
+    if(randEntertainment !== entertainmentNotWanted){
+       userConfirm = prompt(`Your entertainment is ${randEntertainment}, Is this destination exceptable? (y/n):`);
+    }
+    if (userConfirm == "y") {
     confirmed = true;
     return randEntertainment;
-    } 
+    } else {
+      entertainmentNotWanted = randEntertainment;
+    }
   }
 }
 // Ending function that puts it all together. 
